dd69977930ef359e9012524f8ccd585b83907aaa
Daniel Falbel <dfalbel@gmail.com>
2016-09-12 21:52:53 -0400

    moved this to another file

--- data-raw/01-download-wikipedia-pages.R
+++ data-raw/01-download-wikipedia-pages.R
@@ -9,22 +9,3 @@ for (i in 1:10000) {
   download_wikipedia(path = "data-raw/wikipages/")
   Sys.sleep(1)
 }
-
-words <- list.files("data-raw/wikipages/", full.names = T) %>%
-  plyr::llply(function(x){
-    read_html(x) %>%
-      html_nodes("p") %>%
-      html_text()
-  }, .progress = "text") %>%
-  unlist() %>%
-  stringr::str_extract_all("\\b[:alpha:]+\\b") %>%
-  unlist() %>%
-  tolower() %>%
-  dplyr::data_frame(word = .) %>%
-  dplyr::group_by(word) %>%
-  dplyr::summarise(count = n()) %>%
-  dplyr::arrange(desc(count))
-
-
-
-
