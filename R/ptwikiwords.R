09a62ade0271f8c5a1014384811d77ed83a12b14
Daniel Falbel <dfalbel@gmail.com>
2016-09-12 21:51:36 -0400

    documenting the dataset

---
+++ R/ptwikiwords.R
@@ -0,0 +1,13 @@
+#' ptwikiwords
+#'
+#' Dataset containing words found when randomly downloaded 12.000
+#' pages from \url{pt.wikipedia.org}.
+#'
+#' @format A data frame with 146444 rows and 3 variables:
+#' \describe{
+#'   \item{word}{word, as is, found in Wikipedia pages}
+#'   \item{count}{number of times the word was found in the sample of Wikipedia pages}
+#'   \item{check}{wheter the word existis in the portuguese language (\code{hunspell_check} result)}
+#' }
+#'
+"ptwikiwords"
